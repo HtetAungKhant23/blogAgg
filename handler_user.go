dd6d730210a1a876f9f143d9a5b04ff650f20793
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-04-03 10:45:51 +0630

    feat(model): change database user to json User struct in response

--- handler_user.go
+++ handler_user.go
@@ -38,7 +38,7 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	responseWithJSON(w, 201, user)
+	responseWithJSON(w, 201, databaseUserToUser(user))
 }
 
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
@@ -55,5 +55,5 @@ func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	responseWithJSON(w, 200, user)
+	responseWithJSON(w, 200, databaseUserToUser(user))
 }
