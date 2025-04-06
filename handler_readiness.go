a8893a7fe8e55b35c99d9136e6ace1a6a80d0020
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-04-06 15:49:42 +0630

    feat(healthz): add message and time json response in healthz api

--- handler_readiness.go
+++ handler_readiness.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	responseWithJSON(w, 200, struct{}{})
+	responseWithJSON(w, 200, struct {
+		Message string    `json:"message"`
+		Time    time.Time `json:"time"`
+	}{
+		Message: "server is live",
+		Time:    time.Now(),
+	})
 }
